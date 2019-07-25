794f90e9f12061212281042a15b6cb6206752d14
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 23:39:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-  programmer_hash[:yukihiro_matsumoto]
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby"}
+  programmer_hash[:yukihiro_matsumoto] = {:languages => ["LISP", "C"]}
   
     
 end
