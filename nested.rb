31540d4e4e247fa33fe7ef3cee62bd25702c0c3f
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 23:54:40 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -69,6 +69,7 @@ def adding_matz
 
 	programmer_hash = 
  		{
+        :yukihiro_matsumoto => {},
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
