9891486a2018d2236b50a9682a1af27c819cade6
Victor Jian Choe <vchoe89@gmail.com>
2019-07-25 23:17:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -54,10 +54,10 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
-    return programmer_hash[:dennis_ritchie][:languages]
+     programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
